1ca22442e06ab9ade57b525087b8056ca0985f68
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2021-08-28 17:53:22 +0800

    Draft pC testcases.

--- pC/gen/genmain.py
+++ pC/gen/genmain.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3.6
 
-import os
-print('#', os.getcwd())
-
 from pathlib  import Path
+import random
 import subprocess
 
 from _gen  import runTest
 from _gen.generator  import Generator, genStart
 from _gen.present  import Entry, List, SpaceContainer, LineContainer
 
+from genstruct  import genRandomize, genAlternate
 
 GENDIR = Path(__file__).resolve().parent
 
@@ -40,8 +39,8 @@ class TestGenerator(Generator):
 		super().newTest(*args, **kwargs, native_solve=True)
 
 def genAll(genfunc):
-	def wrapfunc():
-		Q, Qs = genfunc()
+	def wrapfunc(**kwargs):
+		Q, Qs = genfunc(**kwargs)
 		
 		assert( 1 <= Q <= 10**6 )
 		for qx, *qxv in Qs:
@@ -62,10 +61,10 @@ def genAll(genfunc):
 	
 	return wrapfunc
 
-@genAll
 def gen1(genfunc):
-	def wrapfunc():
-		Q, Qs = genfunc()
+	@genAll
+	def wrapfunc(**kwargs):
+		Q, Qs = genfunc(**kwargs)
 		
 		assert( Q <= 8000 )
 		for qx, *qxv in Qs:
@@ -75,10 +74,10 @@ def gen1(genfunc):
 	
 	return wrapfunc
 
-@genAll
 def gen2(genfunc):
-	def wrapfunc():
-		Q, Qs = genfunc()
+	@genAll
+	def wrapfunc(**kwargs):
+		Q, Qs = genfunc(**kwargs)
 		
 		for qx, *qxv in Qs:
 			if qx == 2:
@@ -89,10 +88,10 @@ def gen2(genfunc):
 	
 	return wrapfunc
 
-@genAll
 def gen3(genfunc):
-	def wrapfunc():
-		Q, Qs = genfunc()
+	@genAll
+	def wrapfunc(**kwargs):
+		Q, Qs = genfunc(**kwargs)
 		
 		for qx, *qxv in Qs:
 			if qx == 1:
@@ -105,10 +104,10 @@ def gen3(genfunc):
 	
 	return wrapfunc
 
-@genAll
 def gen4(genfunc):
-	def wrapfunc():
-		Q, Qs = genfunc()
+	@genAll
+	def wrapfunc(**kwargs):
+		Q, Qs = genfunc(**kwargs)
 		
 		for qx, *qxv in Qs:
 			assert( qx != 3 )
@@ -119,8 +118,10 @@ def gen4(genfunc):
 
 @genAll
 def genSample1():
-	return [
-		8, [
+	return \
+	[
+		8,
+		[
 			[1, 3],
 			[2, 1, '+', 2],
 			[1, 3],
@@ -134,8 +135,10 @@ def genSample1():
 
 @genAll
 def genSample2():
-	return [
-		9, [
+	return \
+	[
+		9,
+		[
 			[1, 3],
 			[2, 1, '*', 0],
 			[1, 3],
@@ -148,16 +151,97 @@ def genSample2():
 		]
 	]
 
+@gen1
+def gen1Random(**kwargs):
+	Q = random.randint(7000, 8000)
+	return [Q, genRandomize(Q, qxweight=(1, 1, 0), **kwargs)]
+
+@gen1
+def gen1Alter(**kwargs):
+	Q = random.randint(7700, 8000)
+	return [Q, genAlternate(Q, has3=False, **kwargs)]
+
+@gen2
+def gen2Random(**kwargs):
+	Q = random.randint(800000, 10**6)
+	return [Q, genRandomize(Q, qxweight=(1, 1, 1), opweight=(1, 0), **kwargs)]
+
+@gen2
+def gen2Alter(**kwargs):
+	Q = random.randint(950000, 10**6)
+	return [Q, genAlternate(Q, opweight=(1, 0), **kwargs)]
+
+@gen3
+def gen3Random(**kwargs):
+	Q = random.randint(800000, 10**6)
+	return [Q, genRandomize(Q, krange=(0, 1), qxweight=(1, 1, 0), **kwargs)]
+
+@gen3
+def gen3Alter(**kwargs):
+	Q = random.randint(950000, 10**6)
+	return [Q, genAlternate(Q, krange=(0, 1), has3=False, **kwargs)]
+
+@gen4
+def gen4Random(**kwargs):
+	Q = random.randint(800000, 10**6)
+	return [Q, genRandomize(Q, qxweight=(1, 1, 0), **kwargs)]
+
+@gen4
+def gen4Alter(**kwargs):
+	Q = random.randint(950000, 10**6)
+	return [Q, genAlternate(Q, has3=False, **kwargs)]
+
+@genAll
+def gen5Random(**kwargs):
+	Q = random.randint(800000, 10**6)
+	return [Q, genRandomize(Q, **kwargs)]
+
+@genAll
+def gen5Alter(**kwargs):
+	Q = random.randint(950000, 10**6)
+	return [Q, genAlternate(Q, **kwargs)]
+
 def genMain():
-	test_format = str(GENDIR.resolve() / 'manual' / '{subtask}-{testid}.{ext}')
+	random.seed(20210828**1327)
+	
+	test_format = str(GENDIR.resolve() / 'manual' / '{subtask}-{testid:0>2}.{ext}')
 	
 	gen = TestGenerator()
 	gen.config(debug=False, test_format=test_format)
 	
+	print('[genMain]: Gen #samples')
 	gen.newTest('samples', 1, genSample1)
 	gen.newTest('samples', 1, genSample2)
 	
-	# sub1 sub2 sub3 sub4 all
+	print('[genMain]: Gen #sub1')
+	gen.newTest('sub1', 3, gen1Random)
+	gen.newTest('sub1', 2, gen1Alter)
+	gen.newTest('sub1', 1, gen1Alter, opt_gen=dict(opweight=(1, 100)))
+	gen.newTest('sub1', 1, gen1Alter, opt_gen=dict(opweight=(100, 1)))
+	
+	print('[genMain]: Gen #sub2')
+	gen.newTest('sub2', 3, gen2Random)
+	gen.newTest('sub2', 3, gen2Alter)
+	
+	print('[genMain]: Gen #sub3')
+	gen.newTest('sub3', 3, gen3Random)
+	gen.newTest('sub3', 2, gen3Alter)
+	gen.newTest('sub3', 1, gen3Alter, opt_gen=dict(opweight=(1, 100)))
+	gen.newTest('sub3', 1, gen3Alter, opt_gen=dict(opweight=(100, 1)))
+	
+	print('[genMain]: Gen #sub4')
+	gen.newTest('sub4', 3, gen4Random)
+	gen.newTest('sub4', 2, gen4Alter)
+	gen.newTest('sub4', 1, gen4Alter, opt_gen=dict(opweight=(1, 100)))
+	gen.newTest('sub4', 1, gen4Alter, opt_gen=dict(opweight=(100, 1)))
+	
+	print('[genMain]: Gen #all')
+	gen.newTest('all', 3, gen5Random)
+	gen.newTest('all', 1, gen5Random, opt_gen=dict(qxweight=(10, 10, 1)))
+	gen.newTest('all', 1, gen5Random, opt_gen=dict(qxweight=(1, 1, 3)))
+	gen.newTest('all', 2, gen5Alter)
+	gen.newTest('all', 1, gen5Alter, opt_gen=dict(opweight=(1, 100)))
+	gen.newTest('all', 1, gen5Alter, opt_gen=dict(opweight=(100, 1)))
 	
 	runTest.config(test_dir=str(GENDIR.resolve() / 'manual'))
 	with open('data', 'w') as gendata:
