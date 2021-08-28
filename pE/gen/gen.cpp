ad57dcbca0c887356bebfc4408ec13e1a5e39431
lys0829 <lys0829.cs08@nctu.edu.tw>
2021-08-29 00:17:17 +0800

    pE solution, generator, testcases, statement

--- pE/gen/gen.cpp
+++ pE/gen/gen.cpp
@@ -2,21 +2,67 @@
 #include "testlib.h"
 #define endl '\n'
 using namespace std;
-char sign[] = {'+', '-', '*', '/'};
+const int Rmin = 5;
+const int Rmax = 30;
+int NodeLimit, EdgeLimit;
 
-void gen_task2() {
-	int signid = rnd.next(4);
-	int B = rnd.next(1, 10000);
+struct Edge{
+	int from,to,r;
+};
+struct Circuit{
+	int from,to;
+	long double r;
+	vector<int> edges;
+	Circuit(){}
+	Circuit(int from,int to,long double r,int edge):from(from),to(to),r(r){edges.push_back(edge);}
+};
+vector<Circuit> C;
+vector<Edge> E;
+int count_C, count_E, count_N;
+int NodeID;
+map<int,int> nodemap;
 
-	cout << 0 << " " << sign[signid] << " " << B << endl;
+void merge_parallel(int ca,int cb){
+	for(int e:C[cb].edges){
+		if(E[e].from==C[cb].from)E[e].from = C[ca].from;
+		if(E[e].to==C[cb].to)E[e].to = C[ca].to;
+		C[ca].edges.push_back(e);
+	}
+	C[ca].r = 1.0/(1.0/C[ca].r + 1.0/C[cb].r);
+	C.erase(C.begin()+cb,C.begin()+cb+1);
+	count_C--;
+	count_N-=2;
 }
 
-void gen_norm(long long MIN_V, long long MAX_V) {
-	int A = rnd.next(MIN_V, MAX_V);
-	int signid = rnd.next(4);
-	int B = rnd.next(MIN_V, MAX_V);
+void concat_series(int ca,int cb){
+	for(int e:C[cb].edges){
+		if(E[e].from==C[cb].from)E[e].from = C[ca].to;
+		C[ca].edges.push_back(e);
+	}
+	C[ca].to = C[cb].to;
+	C[ca].r = C[ca].r + C[cb].r;
+	C.erase(C.begin()+cb,C.begin()+cb+1);
+	count_C--;
+	count_N--;
+}
 
-	cout << A << " " << sign[signid] << " " << B << endl;
+void new_edge(){
+	int a,b,r;
+	a = NodeID++;
+	b = NodeID++;
+	r = rnd.next(Rmin, Rmax);
+	C.push_back(Circuit(a,b,r,E.size()));
+	E.push_back(Edge({a,b,r}));
+	count_C++;
+	count_E++;
+	count_N+=2;
+}
+
+int node_remap(int node){
+	if(nodemap.find(node)==nodemap.end()){
+		nodemap[node] = NodeID++;
+	}
+	return nodemap[node];
 }
 
 int main(int argc, char* argv[]) {
@@ -24,16 +70,38 @@ int main(int argc, char* argv[]) {
 	cin.tie(0);
 	registerGen(argc, argv, 1);
 
-	string MODE = argv[1];
+	// gen {Nodes} {Edges} {S(eries)/P(arallel)/B(oth)} {seed}
+	NodeLimit = atoi(argv[1]);
+	EdgeLimit = atoi(argv[2]);
 
-	if (MODE == "norm") {
-		long long MIN_V = atoll(argv[2]);
-		long long MAX_V = atoll(argv[3]);
+	int model = 1, moder = 2;
+	if(argv[3][0]=='S') moder = 1;
+	else if(argv[3][0]=='P') model = 2;
 
-		gen_norm(MIN_V, MAX_V);
-	} else if (MODE == "task2") {
-		gen_task2();
-	} else {
-		return -1;
+	while((count_E<EdgeLimit&&count_N+2<=NodeLimit) || count_C>1){
+		int op1 = rnd.next(1,((count_N+2<=NodeLimit&&count_E<EdgeLimit)?2:1));
+		if(op1==1 && count_C>1){ //merge
+			int op2 = rnd.next(model,moder);
+			shuffle(C.begin(),C.end());
+			if(op2==1){
+				concat_series(C.size()-2,C.size()-1);
+			}
+			else if(op2==2){
+				merge_parallel(C.size()-2,C.size()-1);
+			}
+		}
+		else if(op1==2){ //new edge
+			new_edge();
+		}
 	}
+
+	NodeID = 3;
+	nodemap[C[0].from] = 1;
+	nodemap[C[0].to] = 2;
+
+	cout<<count_N<<" "<<count_E<<endl;
+	for(Edge e:E){
+		cout<<node_remap(e.from)<<" "<<node_remap(e.to)<<" "<<e.r<<endl;
+	}
+	//cout<<setprecision(10)<<C[0].r<<endl;
 }
--- pE/validator/validator.cpp
+++ pE/validator/validator.cpp
@@ -1,19 +1,43 @@
 #include <bits/stdc++.h>
 #include "testlib.h"
-
 using namespace std;
+struct Node{
+	vector<int> to;
+} G[1000010];
+bool visited[1000010],leaved[1000010];
+
+bool dfs_check_DAG(int id){
+	if(visited[id]&&!leaved[id]) return false;
+	if(visited[id]) return true;
+	visited[id] = true;
+	for(int t:G[id].to){
+		bool c = dfs_check_DAG(t);
+		if(!c) return false;
+	}
+	//visited[id] = false;
+	leaved[id] = true;
+	return true;
+}
 
 int main() {
 	registerValidation();
 
-	int A = inf.readInt(-1000000000, 1000000000);
+	int N = inf.readInt(1, 1000000);
 	inf.readSpace();
-	inf.readChar();
-	inf.readSpace();
-	int B = inf.readInt(-1000000000, 1000000000);
+	int M = inf.readInt(1, 1000000);
 	inf.readEoln();
-
+	for(int i=0;i<M;i++){
+		int a,b,r;
+		a = inf.readInt(1,N);
+		inf.readSpace();
+		b = inf.readInt(1,N);
+		inf.readSpace();
+		r = inf.readInt(1,30);
+		inf.readEoln();
+		G[a].to.push_back(b);
+	}
+	//inf.readEoln();
 	inf.readEof();
-
+	dfs_check_DAG(1);
 	return 0;
 }
