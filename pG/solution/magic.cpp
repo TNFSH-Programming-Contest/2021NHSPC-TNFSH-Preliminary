1d2994b06aa34aedd06e3d56e45310889a296d91
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-09 18:11:03 +0800

    pG Add magic.cpp

---
+++ pG/solution/magic.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	mt19937 mt1(7296546);
+	for (int i = 0; i < 5; i++) {
+		printf("%c", mt1() % 26 + 97);
+	}
+	printf(", ");
+
+	mt19937 mt2(21535470);
+	for (int i = 0; i < 5; i++) {
+		printf("%c", mt2() % 26 + 97);
+	}
+	printf("\n");
+}
