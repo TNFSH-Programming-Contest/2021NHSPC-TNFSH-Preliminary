a96ee4063df4c51f85fe17e6c27acd54dcb67487
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-10 11:13:03 +0800

    pG Add wrong-case.cpp

---
+++ pG/solution/wrong-case.cpp
@@ -0,0 +1,14 @@
+// wrong-case.cpp
+// By xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << "Hello, World" << endl;
+
+	return 0;
+}
