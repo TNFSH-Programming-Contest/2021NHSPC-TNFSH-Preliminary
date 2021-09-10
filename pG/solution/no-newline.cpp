29756c6e8a8ac13a092a9df8b53dc40d7a71e1cc
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-10 10:07:53 +0800

    pG Add more solutions

---
+++ pG/solution/no-newline.cpp
@@ -0,0 +1,14 @@
+// no-newline.cpp
+// By xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << "hello, world";
+
+	return 0;
+}
